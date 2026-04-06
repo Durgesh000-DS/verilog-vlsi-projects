113e71b96da157acf6710cc6fe2807d7a52885ac
Durgesh Savale <durgeshsavale2@gmail.com>
2026-04-06 12:11:02 +0530

    Implement testbench for half adder
    Added testbench for half adder functionality.

--- half_adder/half_adder.sv
+++ half_adder/half_adder.sv
@@ -1,11 +1,18 @@
-module half_adder (
-  input  a,
-  input  b,
-  output sum,
-  output carry
-);
+module tb;
+  reg a, b;
+  wire sum, carry;
 
-  assign sum  = a ^ b;  // XOR gate
-  assign carry = a & b;  // AND gate
+  half_adder uut (.a(a), .b(b), .sum(sum), .carry(carry));
 
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb);
+
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+
+    $finish;
+  end
 endmodule
